53d25e91fbf9cd412df156d239931737a6091a47
ultraduce-hub <thomaspizzurro50@gmail.com>
2025-08-18 12:30:31 +0200

    Add files via upload

---
+++ Esercitazioni_svolte/pizzu switch.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+int main(){
+	
+	int numero;
+	
+	cout << "inserisci un numero:";
+	
+	cin >> numero;
+	
+	
+	switch(numero){
+		
+		case 1:
+			
+		 	cout << "upper";
+		 	
+		 	break;
+		 	
+		case 2:
+			 
+			 cout << "see you again";
+			 
+			 break;
+			 
+		case 3:	 
+		
+			 cout << "die hard";
+			 
+			 break;
+			 
+		case 4:
+			 
+			 cout << "i really wamt tp stay at your house";
+			 
+			 break;
+			 
+		case 5:
+				
+				
+			 cout << "90210";
+			 
+			 	break;
+			 	
+			 
+		  default:
+		  	
+		  	cout << "errore";
+			
+	}
+	
+	
+	
+	
+	
+	
+	
+}
